ed5f43889a449687f83c1db0b13982aad0a8ea20
Matan Yosef <matan.yosef02@gmail.com>
2025-10-31 19:20:00 +1300

    feat: add webpack support to preset addon
    Implements the `webpackFinal` function in the preset addon to support webpack configuration.

--- src/addon/preset.ts
+++ src/addon/preset.ts
@@ -3,7 +3,7 @@ import {
     PresetValue,
     StorybookConfigRaw,
 } from 'storybook/internal/types';
-import { vite } from './unplugin';
+import { vite, webpack } from './unplugin';
 import { configIndexer } from './indexers';
 import { cssIndexer } from './indexers';
 import { ThemeLoaderManager } from './core/theme-loader';
@@ -22,4 +22,13 @@ export const viteFinal = async (config: any, options: any) => {
     return config;
 };
 
-// TODO: Add webpack support
+export const webpackFinal = async (config: any, options: any) => {
+    const { plugins = [] } = config;
+    const stories = await options.presets.apply('stories');
+    const themeLoaderManager = new ThemeLoaderManager(stories);
+    const themeLoader = themeLoaderManager.getLoader();
+    if (themeLoader === null) return config;
+    plugins.push(webpack({ ...options, themeLoader }));
+    config.plugins = plugins;
+    return config;
+};
